9dc26193633b5f67585c4f73eb9eda7c19d7ba41
Yashesh Bhatt <yashesh.bhatt@neosofttech.com>
2024-09-13 18:56:25 +0530

    init commit with helm

--- src/main/java/com/bupa/sadir/leads/SadirLeadsApplication.java
+++ src/main/java/com/bupa/sadir/leads/SadirLeadsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SadirLeadsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SadirLeadsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SadirLeadsApplication.class, args);
+    }
 
 }
--- src/test/java/com/bupa/sadir/leads/SadirLeadsApplicationTests.java
+++ src/test/java/com/bupa/sadir/leads/SadirLeadsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SadirLeadsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
